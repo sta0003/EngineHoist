c6f344eac640147d4aa76898bc104e661e453bfc
BuildTools <unconfigured@null.spigotmc.org>
2022-11-02 14:48:47 +1100

    analytics implimented correctly

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ configLocation      = "./hoist.cfg"
 themesLocation      = "./assets/themes/*.mr"
 appLocation         = "engine-sim-app.exe"
 mainDir             = os.getcwd()
-version             = "1.5.0"
+version             = "1.4.1"
 engineLocations     = []
 failedEngines       = []
 
